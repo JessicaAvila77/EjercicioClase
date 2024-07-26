a774fe63ffb0b770226535dd01918b4a0747e869
JessicaAvila77 <jessica.avilaunah@gmail.com>
2024-07-25 20:36:56 -0600

    Creación de la clase SuperPoder, se instancia en la clase SuperHeroe

--- SuperHeroe.cs
+++ SuperHeroe.cs
@@ -12,8 +12,28 @@ namespace EjercicioClase
         public string IdentidadSecreta { get; set; }
         public string Ciudad {  get; set; }
         public bool PuedeVolar { get; set; }
+        //referencia a la clase SuperPoder
+        public SuperPoder SuperPoder { get; set; }
 
+        //Constructor
+        public  SuperHeroe()
+        {
+            PuedeVolar = true;
+            //cada vez que cree un nuevo objeto el inicializará la clase
+            SuperPoder = new SuperPoder();
+        }
 
+        public void Imprimir()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Identidad Secreta: {IdentidadSecreta}");
+            Console.WriteLine($"Ciudad: {Ciudad}");
+            Console.WriteLine($"Puede Volar: {(PuedeVolar ? "Si" : "No")}");
+            Console.WriteLine($"Super poder: {SuperPoder.Nombre}");
+            Console.WriteLine($"Descripción: {SuperPoder.Descripcion}");
+            Console.WriteLine($"Nivel: {SuperPoder.Nivel}");
+
+        }
 
 
     }
---
+++ SuperPoder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase
+{
+    public class SuperPoder
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Nivel { get; set; }
+
+
+    }
+}
